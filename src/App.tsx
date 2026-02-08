d1735613f50a9d4e59b34996dab56d8b93bab578
lauren_m <laurenamolloy@gmail.com>
2026-02-08 21:31:54 +0200

    Fixed TS alias resolution by enabling composite in tsconfig.app.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router";
-import Root from "./pages/Root";
-import HomePage from "./pages/home/HomePage";
-import DetailsPage from './pages/details/DetailsPage'
-import CartPage from "./pages/cart/CartPage";
-import MenuPage from "./pages/menu/MenuPage";
-import { menuLoader } from "./pages/menu/menuLoader";
-import detailsLoader from "./pages/details/detailsLoader";
-import { CartProvider } from "./context/CartProvider";
+import Root from "@/pages/Root";
+import HomePage from "@/pages/home/HomePage";
+import DetailsPage from '@/pages/details/DetailsPage';
+import CartPage from "@/pages/cart/CartPage";
+import MenuPage from "@/pages/menu/MenuPage";
+import { menuLoader } from "@/pages/menu/menuLoader";
+import detailsLoader from "@/pages/details/detailsLoader";
+import { CartProvider } from "@/context/CartProvider";
 
 //SET UP BROWSER ROUTER
 const router = createBrowserRouter([
