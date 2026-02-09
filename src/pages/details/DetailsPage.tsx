9c31cfd71597d63391aa53c0a3d91dbe49fffebe
lauren_m <laurenamolloy@gmail.com>
2026-02-09 10:08:11 +0200

    Restore DetailsPage with correct casing

--- src/pages/details/DetailsPage.tsx
+++ src/pages/details/DetailsPage.tsx
@@ -1,33 +1,35 @@
 import { useLoaderData } from 'react-router-dom';
-import type { DetailsLoaderResult} from '@/pages/details/detailsLoader'
-import ProductGallery from '@/components/ProductGallery';
-import ProductInfo from '@/components/ProductInfo';
-import AddToCartSection from '@/components/AddToCartSection';
+import type { DetailsLoaderResult } from './detailsLoader';
+import ProductGallery from '../../components/ProductGallery';
+import ProductInfo from '../../components/ProductInfo';
+import AddToCartSection from '../../components/AddToCartSection';
 
 export default function DetailsPage() {
-  const { item } = useLoaderData() as DetailsLoaderResult
-  //console.log(item)
-  const { 
+  const { item } = useLoaderData() as DetailsLoaderResult;
+
+  const {
     image,
     name,
     description,
     price,
-  } = item
+  } = item;
 
   return (
     <div className="container flex flex-col md:flex-row md:gap-10 justify-center items-center mt-10 mb-10 px-5 gap-3">
-      <div className='flex flex-col'>
-      <ProductGallery
-      name={name}
-      image={image} />
+      <div className="flex flex-col">
+        <ProductGallery
+          name={name}
+          image={image}
+        />
       </div>
       <div className="flex flex-col gap-5">
         <ProductInfo
-        name={name}
-        description={description}
-        price={price}  />
+          name={name}
+          description={description}
+          price={price}
+        />
         <AddToCartSection product={item} />
       </div>
     </div>
-  )
+  );
 }
